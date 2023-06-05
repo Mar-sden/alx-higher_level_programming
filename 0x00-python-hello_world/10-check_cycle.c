c9453642fa7c278d668599831a69dbe6ca2054b5
Mar-sden <huncho8956@gmail.com>
2023-06-05 03:41:07 -0700

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,52 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 
-// Definition of a node in the linked list
-typedef struct Node {
-    int data;
-    struct Node* next;
-} Node;
+/**
+ * check_cycle - checks if a singly linked list has
+ * a cycle in it
+ * @list: pointer to the list
+ * Return: 0 if there is no cycle,
+ * 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *p3;
+	listint_t *prev;
 
-// Function to check if a linked list has a cycle
-int check_cycle(Node* list) {
-    Node* slow = list; // Slow pointer moves one step at a time
-    Node* fast = list; // Fast pointer moves two steps at a time
+	p3 = list;
+	prev = list;
+	while (list && p3 && p3->next)
+	{
+		list = list->next;
+		p3 = p3->next->next;
 
-    while (fast != NULL && fast->next != NULL) {
-        slow = slow->next;
-        fast = fast->next->next;
+		if (list == p3)
+		{
+			list = prev;
+			prev =  p3;
+			while (1)
+			{
+				p3 = prev;
+				while (p3->next != list && p3->next != prev)
+				{
+					p3 = p3->next;
+				}
+				if (p3->next == list)
+					break;
 
-        // If there is a cycle, the fast pointer will eventually catch up with the slow pointer
-        if (slow == fast) {
-            return 1; // Cycle detected
-        }
-    }
+				list = list->next;
+			}
+			return (1);
+		}
+	}
 
-    return 0; // No cycle found
+	return (0);
 }
-
-// Test the function
-int main() {
-    // Create a linked list with a cycle
-    Node* head = (Node*)malloc(sizeof(Node));
-    head->data = 1;
-    head->next = (Node*)malloc(sizeof(Node));
-    head->next->data = 2;
-    head->next->next = (Node*)malloc(sizeof(Node));
-    head->next->next->data = 3;
-    head->next->next->next = head; // Create a cycle by pointing the last node to the head
-
-    // Call the function to check for a cycle
-    int hasCycle = check_cycle(head);
-
-    // Print the result
-    printf("The linked list %s a cycle.\n", hasCycle ? "has" : "does not have");
-
-    // Clean up the memory
-    free(head->next->next);
-    free(head->next);
-    free(head);
-
-    return 0;
-}
-
